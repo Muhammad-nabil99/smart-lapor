81b841075eec130c4703c861011ca3473b238c54
Muhammad-nabil99 <muhammad.nabil290303@gmail.com>
2025-05-30 23:51:31 +0800

    testing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "flowbite-react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Table from "./components/Table";
+
+function App() {
+  return (
+    <div>
+      {/* <Table/> */}
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
