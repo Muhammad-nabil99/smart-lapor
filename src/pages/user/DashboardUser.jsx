3597a4c2cbbee62a73be73b95e3f59e79a1af3be
ahmadadptr001 <ahmadadptr@gmail.com>
2025-05-31 07:33:42 +0700

    menambahkan konten di dalam dashboard user (home)

--- src/pages/user/DashboardUser.jsx
+++ src/pages/user/DashboardUser.jsx
@@ -1,8 +1,197 @@
-import SidebarUser from "../../component/user/SidebarUser";
+import imageCard from "../../assets/contoh.png"
 
 export default function DashboardUser() {
         return (
-                <>
-                </>
+                <div className="p-4">
+                        
+                        <div className="grid grid-cols-2 gap-7">
+                        
+                                <div className="card rounded-md shadow-sm/10 bg-[#FBFBFB]">
+                                        <div className="card-header h-[200px]">
+                                                <img src={imageCard} alt="AI - kecerdasan buatan" className="w-full h-full object-cover"/>
+                                        </div>
+                                        <div className="card-body p-4">
+                                                <h5 className="text-base font-bold tracking-tight text-gray-900 dark:text-white">
+                                                        Smart Lapor
+                                                </h5>
+                                                <p className="mt-3 font-normal text-lg  dark:text-gray-400">
+                                                        Selamat datanga john dor
+                                                </p>
+                                        </div>
+                                </div>
+
+                                <div id="laporan-user">
+                                        <span className="font-extrabold text-xl text-gray-700">Laporan Kamu</span>
+                                        <div className="card rounded-md shadow-md/30 mt-3">
+                                                <div className="card-body p-4">
+                                                        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                                Pungli Jalanan
+                                                        </h5>
+                                                        <div className="rounded-full gap-2 bg-pending p-2 px-3 flex items-center w-fit mt-5">
+                                                                <i className="fas fa-circle text-dot-pending text-sm"></i>
+                                                                <span className="text-white text-[.7rem]">Pending</span>
+                                                        </div>
+                                                        <p className="mt-2 text-sm">
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit nihil delectus enim soluta a animi vel rerum, praesentium voluptate!
+                                                        </p>
+                                                        <p className="text-sm mt-5">
+                                                                <i className="fas fa-location-dot"></i> Jl. Wolasi abc
+                                                        </p>
+                                                </div>
+                                                <div className="card-footer flex items-center justify-between p-5 text-sm">
+                                                        <a href="#" className="hover:underline text-sky-600">Lihat ></a>
+                                                        <span>
+                                                                <i>29/02/2025</i>
+                                                        </span>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                        
+                        <div id="laporan-user-lain" className="mt-10">
+                                <span className="font-semibold text-base text-gray-600">Lihat Laporan Orang Lain</span>
+                                <div  className="overflow-x-auto flex gap-5 mt-3 w-[65vw] sm:w-[75vw] md:w-[80vw] lg:w-[85vw] py-2 ">
+                                        <div className="card rounded-md shadow-md/30 ">
+                                                <div className="card-body p-4 w-[600px]">
+                                                        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                                Pungli Jalanan
+                                                        </h5>
+                                                        <div className="rounded-full gap-2 bg-pending p-2 px-3 flex items-center w-fit mt-5">
+                                                                <i className="fas fa-circle text-dot-pending text-sm"></i>
+                                                                <span className="text-white text-[.7rem]">Pending</span>
+                                                        </div>
+                                                        <p className="mt-2 text-sm">
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit nihil delectus enim soluta a animi vel rerum, praesentium voluptate!
+                                                        </p>
+                                                        <p className="text-sm mt-5">
+                                                                <i className="fas fa-location-dot"></i> Jl. Wolasi abc
+                                                        </p>
+                                                </div>
+                                                <div className="card-footer flex items-center justify-between p-5 text-sm">
+                                                        <a href="#" className="hover:underline text-sky-600">Lihat ></a>
+                                                        <span>
+                                                                <i>29/02/2025</i>
+                                                        </span>
+                                                </div>
+                                        </div>
+                                        <div className="card rounded-md shadow-md/30">
+                                                <div className="card-body p-4 w-[600px]">
+                                                        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                                Pungli Jalanan
+                                                        </h5>
+                                                        <div className="rounded-full gap-2 bg-pending p-2 px-3 flex items-center w-fit mt-5">
+                                                                <i className="fas fa-circle text-dot-pending text-sm"></i>
+                                                                <span className="text-white text-[.7rem]">Pending</span>
+                                                        </div>
+                                                        <p className="mt-2 text-sm">
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit nihil delectus enim soluta a animi vel rerum, praesentium voluptate!
+                                                        </p>
+                                                        <p className="text-sm mt-5">
+                                                                <i className="fas fa-location-dot"></i> Jl. Wolasi abc
+                                                        </p>
+                                                </div>
+                                                <div className="card-footer flex items-center justify-between p-5 text-sm">
+                                                        <a href="#" className="hover:underline text-sky-600">Lihat ></a>
+                                                        <span>
+                                                                <i>29/02/2025</i>
+                                                        </span>
+                                                </div>
+                                        </div>
+                                        <div className="card rounded-md shadow-md/30">
+                                                <div className="card-body p-4 w-[600px]">
+                                                        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                                Pungli Jalanan
+                                                        </h5>
+                                                        <div className="rounded-full gap-2 bg-pending p-2 px-3 flex items-center w-fit mt-5">
+                                                                <i className="fas fa-circle text-dot-pending text-sm"></i>
+                                                                <span className="text-white text-[.7rem]">Pending</span>
+                                                        </div>
+                                                        <p className="mt-2 text-sm">
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit nihil delectus enim soluta a animi vel rerum, praesentium voluptate!
+                                                        </p>
+                                                        <p className="text-sm mt-5">
+                                                                <i className="fas fa-location-dot"></i> Jl. Wolasi abc
+                                                        </p>
+                                                </div>
+                                                <div className="card-footer flex items-center justify-between p-5 text-sm">
+                                                        <a href="#" className="hover:underline text-sky-600">Lihat ></a>
+                                                        <span>
+                                                                <i>29/02/2025</i>
+                                                        </span>
+                                                </div>
+                                        </div>
+                                        <div className="card rounded-md shadow-md/30">
+                                                <div className="card-body p-4 w-[600px]">
+                                                        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                                Pungli Jalanan
+                                                        </h5>
+                                                        <div className="rounded-full gap-2 bg-pending p-2 px-3 flex items-center w-fit mt-5">
+                                                                <i className="fas fa-circle text-dot-pending text-sm"></i>
+                                                                <span className="text-white text-[.7rem]">Pending</span>
+                                                        </div>
+                                                        <p className="mt-2 text-sm">
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit nihil delectus enim soluta a animi vel rerum, praesentium voluptate!
+                                                        </p>
+                                                        <p className="text-sm mt-5">
+                                                                <i className="fas fa-location-dot"></i> Jl. Wolasi abc
+                                                        </p>
+                                                </div>
+                                                <div className="card-footer flex items-center justify-between p-5 text-sm">
+                                                        <a href="#" className="hover:underline text-sky-600">Lihat ></a>
+                                                        <span>
+                                                                <i>29/02/2025</i>
+                                                        </span>
+                                                </div>
+                                        </div>
+                                        <div className="card rounded-md shadow-md/30">
+                                                <div className="card-body p-4 w-[600px]">
+                                                        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                                Pungli Jalanan
+                                                        </h5>
+                                                        <div className="rounded-full gap-2 bg-pending p-2 px-3 flex items-center w-fit mt-5">
+                                                                <i className="fas fa-circle text-dot-pending text-sm"></i>
+                                                                <span className="text-white text-[.7rem]">Pending</span>
+                                                        </div>
+                                                        <p className="mt-2 text-sm">
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit nihil delectus enim soluta a animi vel rerum, praesentium voluptate!
+                                                        </p>
+                                                        <p className="text-sm mt-5">
+                                                                <i className="fas fa-location-dot"></i> Jl. Wolasi abc
+                                                        </p>
+                                                </div>
+                                                <div className="card-footer flex items-center justify-between p-5 text-sm">
+                                                        <a href="#" className="hover:underline text-sky-600">Lihat ></a>
+                                                        <span>
+                                                                <i>29/02/2025</i>
+                                                        </span>
+                                                </div>
+                                        </div>
+                                        <div className="card rounded-md shadow-md/30">
+                                                <div className="card-body p-4 w-[600px]">
+                                                        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                                Pungli Jalanan
+                                                        </h5>
+                                                        <div className="rounded-full gap-2 bg-pending p-2 px-3 flex items-center w-fit mt-5">
+                                                                <i className="fas fa-circle text-dot-pending text-sm"></i>
+                                                                <span className="text-white text-[.7rem]">Pending</span>
+                                                        </div>
+                                                        <p className="mt-2 text-sm">
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit nihil delectus enim soluta a animi vel rerum, praesentium voluptate!
+                                                        </p>
+                                                        <p className="text-sm mt-5">
+                                                                <i className="fas fa-location-dot"></i> Jl. Wolasi abc
+                                                        </p>
+                                                </div>
+                                                <div className="card-footer flex items-center justify-between p-5 text-sm">
+                                                        <a href="#" className="hover:underline text-sky-600">Lihat ></a>
+                                                        <span>
+                                                                <i>29/02/2025</i>
+                                                        </span>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+
+                </div>
         )
 }
