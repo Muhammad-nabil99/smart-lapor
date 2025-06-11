baee8af93f1e9814182fcc5084a3645cc77ee485
ahmadadptr001 <ahmadadptr@gmail.com>
2025-06-11 16:35:33 +0700

    Perbaikan ui sidebar dan navbar

--- src/Layout/user/LayoutUser.jsx
+++ src/Layout/user/LayoutUser.jsx
@@ -5,19 +5,17 @@ import NavbarUser from "../../component/user/NavbarUser";
 import { useState } from "react";
 
 export default function LayoutUser() {
-  const [isOpen, setIsOpen] = useState(
-    localStorage.getItem("openSidebarUser") ? "true" : "false"
-  );
-
+    const [darkThemeUser, setDarkThemeUser] = useState(false);
+  
   return (
     <div className="flex flex-nowrap">
       {/* konten sebelah kiri */}
-      <SidebarUser isOpen={isOpen} />
+      <SidebarUser darkThemeUser={darkThemeUser}/>
 
       {/* kontent sebelah kanan */}
       <main className="w-full overflow-x-hidden dark:bg-gray-900">
         {/* navigasi user nya */}
-        <NavbarUser setIsOpen={setIsOpen} />
+        <NavbarUser darkThemeUser={darkThemeUser} setDarkThemeUser={setDarkThemeUser}/>
         
         {/* isi utama nya */}
         <Outlet />
